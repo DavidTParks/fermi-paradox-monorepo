bb2d2c01e50ea7e1af137bce1babee0180d24c40
davidthomasparks <Davidthomasparks@gmail.com>
2023-02-15 17:11:24 -0800

    Remove dead files, prettier, tailwind, prisma init

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "../../packages/ui/components/**/*.{ts,tsx}",
+        "../../apps/fermi-paradox-phaser/src/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
---
+++ prettier.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  tailwindConfig: './packages/config/tailwind.config.js',
+}
